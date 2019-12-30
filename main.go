367d789b27c19a4e785adfd2256b1357f4155976
zhengyiwei <zhengyiwei@bytedance.com>
2019-12-30 17:17:26 +0800

    test int ptr

--- main.go
+++ main.go
@@ -1,43 +1,36 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-	"time"
-)
-
 func main() {
-	bas := &base{
-		f: 4.2,
-		s: "123",
-	}
-	der := &derived{
-		int64: 64,
-		base:  bas,
-	}
-	testStruct()
-	testOOP1(bas)
-	// testOOP2(der) //cannot use der (type *derived) as type *base in argument to testOOP2
-	testOOP3(der)
-	testOOP4(bas)
-	testOOP4(der)
-
-	definedMapSpace(3)
-
-	a, b := retVarDefinedNull(2)
-	fmt.Println(a == nil, b == nil)
-
-	a, b = retVarDefined(2)
-	fmt.Println(a, b) //slice不用make, map要make
-
-	var wg sync.WaitGroup
-	workerCount := 2
-	for i := 0; i < workerCount; i++ {
-		wg.Add(1) //只能是1
-		go testWaitGroup(i, &wg)
-	}
-	time.Sleep(1 * time.Second)
-	wg.Wait()
-	fmt.Println("all done!")
-
+	//bas := &base{
+	//	f: 4.2,
+	//	s: "123",
+	//}
+	//der := &derived{
+	//	int64: 64,
+	//	base:  bas,
+	//}
+	//testStruct()
+	//testOOP1(bas)
+	//// testOOP2(der) //cannot use der (type *derived) as type *base in argument to testOOP2
+	//testOOP3(der)
+	//testOOP4(bas)
+	//testOOP4(der)
+	//
+	//definedMapSpace(3)
+	//
+	//a, b := retVarDefinedNull(2)
+	//fmt.Println(a == nil, b == nil)
+	//
+	//a, b = retVarDefined(2)
+	//fmt.Println(a, b) //slice不用make, map要make
+	//
+	//var wg sync.WaitGroup
+	//workerCount := 2
+	//for i := 0; i < workerCount; i++ {
+	//	wg.Add(1) //只能是1
+	//	go testWaitGroup(i, &wg)
+	//}
+	//time.Sleep(1 * time.Second)
+	//wg.Wait()
+	//fmt.Println("all done!")
 }
