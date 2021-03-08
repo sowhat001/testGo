51b9552aa8b9ebe4587a052ef878d73a98c124d4
sowhat001 <1271417106@qq.com>
2021-03-08 16:53:48 +0800

    test json unmarshal empty

--- json.go
+++ json.go
@@ -5,6 +5,12 @@ import (
 	"fmt"
 )
 
+type test struct {
+	B int
+	C float64
+	D string
+}
+
 type BaseReq struct {
 	PID       string `json:"pid"`
 	BID       string `json:"bid"`
@@ -62,6 +68,90 @@ func TestParseEmpty() {
 	if err != nil {
 		fmt.Println(err)
 	} else {
-		fmt.Print(string(v) == "{}") // true
+		fmt.Println(string(v) == "{}") // true
 	}
+
+	var arrayTest []*test
+	var pointerTest *test
+	var structTest test
+
+	fmt.Println("-------marshal Array-------")
+	bytes, err := json.Marshal(arrayTest)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(bytes) // "null"
+		fmt.Println(string(bytes))
+	}
+
+	fmt.Println("-------unmarshal Array-------")
+	err = json.Unmarshal(bytes, &arrayTest)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(arrayTest) // []
+	}
+
+	fmt.Println("-------marshal pointer-------")
+	bytes, err = json.Marshal(pointerTest)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(bytes) // "null"
+		fmt.Println(string(bytes))
+	}
+
+	fmt.Println("-------unmarshal pointer-------")
+	err = json.Unmarshal(bytes, &pointerTest)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(pointerTest) // []
+	}
+
+	fmt.Println("-------marshal struct-------")
+	bytes, err = json.Marshal(structTest)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(bytes)
+		fmt.Println(string(bytes)) // {"B":0,"C":0,"D":""}
+	}
+
+	fmt.Println("-------unmarshal struct-------")
+	err = json.Unmarshal(bytes, &structTest)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(structTest) // {0 0 }
+	}
+
+	fmt.Println("-------empty string-------")
+	emptyString := ""
+	err = json.Unmarshal([]byte(emptyString), &arrayTest) // error
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(arrayTest)
+	}
+
+	err = json.Unmarshal([]byte(emptyString), &pointerTest) // error
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(arrayTest)
+	}
+
+	err = json.Unmarshal([]byte(emptyString), &structTest) // error
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(arrayTest)
+	}
+	fmt.Println()
+}
+
+func TestSize() {
+	s := "[{\"Id\":0,\"Subject\":7,\"Expressions\":[{\"Id\":0,\"Predicate\":{\"Type\":1,\"Negative\":false},\"Objects\":[{\"Id\":0,\"Mode\":1,\"Type\":2,\"Values\":[\"很敏感词\"],\"Ext\":{}},{\"Id\":0,\"Mode\":1,\"Type\":2,\"Values\":[\"很敏感词\"],\"Ext\":{}},{\"Id\":0,\"Mode\":1,\"Type\":2,\"Values\":[\"很敏感词\"],\"Ext\":{}},{\"Id\":0,\"Mode\":1,\"Type\":2,\"Values\":[\"很敏感词\"],\"Ext\":{}}],\"Logic\":1}],\"Logic\":0}]"
+	fmt.Println(len(s))
 }
--- main.go
+++ main.go
@@ -36,6 +36,7 @@ func main() {
 	// TestParseString()
 	// TestPareMultiLayerStruct()
 	TestParseEmpty()
+	TestSize()
 
 	/* TestMap */
 	// TestMapAndSliceMakeOrNot()
